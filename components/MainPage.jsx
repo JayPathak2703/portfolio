81ef21a0f581a98f5aacd941a40527e2978b74fe
Jay Pathak <158112419+JayPathak2703@users.noreply.github.com>
2024-12-04 08:59:33 -0700

    Update MainPage.jsx

--- components/MainPage.jsx
+++ components/MainPage.jsx
@@ -10,8 +10,6 @@ const MainPage= () =>
             <div className = 'max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
                 <div>
                     <p className='uppercase text:sm tracking-widest text-grey-600 text-bold '>Getting To Know Me !</p>
-                    <h1 className='py-4 text-gray-700'>Greetings, I'm 
-                    <span className='text-[#5651e5]'> Jay Pathak</span></h1>
                     <h1 className='py-4 text-gray-700'>Greetings, I&apos;m 
                     <span className='text-[#5651e5]'> Jay Pathak</span>
                     </h1>
